70af503b2c87be0fbbdc124ef83f3eb44a44e1f8
Micah Kemp <micahk@splunk.com>
2021-06-03 01:33:23 +0000

    [SAS-5966] splunkconfig_role_attributes has cumulative_realtime_search_jobs_quota, cumulative_search_jobs_quota, realtime_search_jobs_quota, search_disk_quota, search_jobs_quota, search_time_win.

--- internal/provider/roleattributes_test.go
+++ internal/provider/roleattributes_test.go
@@ -22,6 +22,23 @@ func TestAccResourceRoleAttributes(t *testing.T) {
 					testCheckResourceAttrList("data.splunkconfig_role_attributes.role_a", "capabilities", []string{
 						"admin_all_objects",
 					}),
+					resource.TestCheckResourceAttr("data.splunkconfig_role_attributes.role_a", "cumulative_realtime_search_jobs_quota", "0"),
+					resource.TestCheckResourceAttr("data.splunkconfig_role_attributes.role_a", "cumulative_search_jobs_quota", "0"),
+					resource.TestCheckResourceAttr("data.splunkconfig_role_attributes.role_a", "realtime_search_jobs_quota", "0"),
+					resource.TestCheckResourceAttr("data.splunkconfig_role_attributes.role_a", "search_disk_quota", "0"),
+					resource.TestCheckResourceAttr("data.splunkconfig_role_attributes.role_a", "search_jobs_quota", "0"),
+					resource.TestCheckResourceAttr("data.splunkconfig_role_attributes.role_a", "search_time_win", "0"),
+				),
+			},
+			{
+				Config: testAccDataSourceRoleAttributesConfigRemovedZeroed,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckNoResourceAttr("data.splunkconfig_role_attributes.role_a", "cumulative_realtime_search_jobs_quota"),
+					resource.TestCheckNoResourceAttr("data.splunkconfig_role_attributes.role_a", "cumulative_search_jobs_quota"),
+					resource.TestCheckNoResourceAttr("data.splunkconfig_role_attributes.role_a", "realtime_search_jobs_quota"),
+					resource.TestCheckNoResourceAttr("data.splunkconfig_role_attributes.role_a", "search_disk_quota"),
+					resource.TestCheckNoResourceAttr("data.splunkconfig_role_attributes.role_a", "search_jobs_quota"),
+					resource.TestCheckNoResourceAttr("data.splunkconfig_role_attributes.role_a", "search_time_win"),
 				),
 			},
 		},
@@ -38,6 +55,38 @@ roles:
     capabilities:
       admin_all_objects: true
       change_authentication: false
+
+    # explicitly settable as zero values
+    cumulativeRTSrchJobsQuota: 0
+    cumulativeSrchJobsQuota: 0
+    rtSrchJobsQuota: 0
+    srchDiskQuota: 0
+    srchJobsQuota: 0
+    srchTimeWin: 0
+
+indexes:
+  - name: index_a_reverse
+    srchRolesAllowed: ["role_a"]
+EOT
+}
+
+data "splunkconfig_role_attributes" "role_a" {
+  role_name = "role_a"
+}
+`
+
+const testAccDataSourceRoleAttributesConfigRemovedZeroed = `
+provider "splunkconfig" {
+	configuration = <<EOT
+roles:
+  - name: role_a
+    srchIndexesAllowed: ["index_a"]
+    importRoles: ["user"]
+    capabilities:
+      admin_all_objects: true
+      change_authentication: false
+
+    # explicitly settable zeroed values removed to ensure they're no longer set by the provider
 
 indexes:
   - name: index_a_reverse
