40a228add049466c15c6e250419ae41d540b21a3
Micah Kemp <micahk@splunk.com>
2021-08-20 17:40:23 -0500

    Added splunkconfig_lookup_attributes.

---
+++ internal/provider/lookupattributes.go
@@ -0,0 +1,92 @@
+// Copyright 2021 Splunk, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package provider
+
+import (
+	"context"
+	"terraform-provider-splunkconfig/internal/splunkconfig/config"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+const (
+	lookupNameKey = "lookup_name"
+	fieldNamesKey = "field_names"
+	rowsKey       = "rows"
+)
+
+func resourceLookupAttributes() *schema.Resource {
+	return &schema.Resource{
+		Description: "Get fields and rows for a specific lookup",
+		ReadContext: resourceLookupAttributesRead,
+		Schema: map[string]*schema.Schema{
+			lookupNameKey: {
+				Description: "Name of the lookup",
+				Type:        schema.TypeString,
+				Required:    true,
+			},
+			fieldNamesKey: {
+				Description: "List of fields in the lookup",
+				Type:        schema.TypeList,
+				Computed:    true,
+				Elem:        &schema.Schema{Type: schema.TypeString},
+			},
+			rowsKey: {
+				Description: "List of rows in the lookup",
+				Type:        schema.TypeList,
+				Computed:    true,
+				Elem:        &schema.Schema{Type: schema.TypeMap},
+			},
+		},
+	}
+}
+
+func resourceLookupAttributesRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	suite := meta.(config.Suite)
+
+	lookupName := d.Get(lookupNameKey).(string)
+
+	d.SetId(lookupName)
+
+	lookup, ok := suite.ExtrapolatedLookups().WithName(lookupName)
+	if !ok {
+		return diag.Errorf("unable to find lookup: %s", lookupName)
+	}
+
+	// fields
+	fieldNames := make([]string, len(lookup.Fields))
+
+	for i, field := range lookup.Fields {
+		fieldNames[i] = field.Name
+	}
+
+	if err := d.Set(fieldNamesKey, fieldNames); err != nil {
+		return diag.FromErr(err)
+	}
+
+	// rows
+	rows := make([]map[string]string, len(lookup.Rows))
+
+	for i, row := range lookup.Rows {
+		rows[i] = map[string]string(row.Values)
+	}
+
+	if err := d.Set(rowsKey, rows); err != nil {
+		return diag.FromErr(err)
+	}
+
+	return diag.Diagnostics{}
+}
---
+++ internal/provider/lookupattributes_test.go
@@ -0,0 +1,65 @@
+// Copyright 2021 Splunk, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package provider
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccResourceLookupFields(t *testing.T) {
+	resource.UnitTest(t, resource.TestCase{
+		ProviderFactories: providerFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceLookupFieldsConfig,
+				Check: resource.ComposeTestCheckFunc(
+					testCheckResourceAttrList("data.splunkconfig_lookup_attributes.foo", "field_names", []string{
+						"field_a",
+						"field_b",
+					}),
+					resource.TestCheckResourceAttr("data.splunkconfig_lookup_attributes.foo", "rows.0.field_a", "row_1_value_a"),
+					resource.TestCheckResourceAttr("data.splunkconfig_lookup_attributes.foo", "rows.0.field_b", "row_1_value_b"),
+					resource.TestCheckResourceAttr("data.splunkconfig_lookup_attributes.foo", "rows.1.field_a", "row_2_value_a"),
+					resource.TestCheckResourceAttr("data.splunkconfig_lookup_attributes.foo", "rows.1.field_b", "row_2_value_b"),
+				),
+			},
+		},
+	})
+}
+
+const testAccDataSourceLookupFieldsConfig = `
+provider "splunkconfig" {
+    configuration = <<EOT
+lookups:
+  - name: test_lookup
+    fields:
+      - name: field_a
+      - name: field_b
+    rows:
+      - values:
+          field_a: row_1_value_a
+          field_b: row_1_value_b
+      - values:
+          field_a: row_2_value_a
+          field_b: row_2_value_b
+EOT
+}
+
+data "splunkconfig_lookup_attributes" "foo" {
+    lookup_name = "test_lookup"
+}
+`
